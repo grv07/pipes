d6450e87916e242f5673f4876b0f52826d3da378
Gaurav Tyagi <grvtyagi22@gmail.com>
2024-07-28 11:42:24 +0530

    Drop pipes will show you messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use log::{debug, error, info, warn};
+use log::{debug, info};
 use log4rs;
 
 use crossterm::{
@@ -17,7 +17,7 @@ use std::{
 struct Point {
     x: u16,
     y: u16,
-    hidden: bool,
+    // hidden: bool,
 }
 
 // type Point = (u16, u16);
@@ -27,14 +27,16 @@ struct Pipe {
     points: VecDeque<Point>,
     length: usize,
     start_point: Point,
+    msg: String,
 }
 
 impl Pipe {
-    fn new(points: VecDeque<Point>, start: Point, length: usize) -> Self {
+    fn new(points: VecDeque<Point>, start: Point, length: usize, msg: String) -> Self {
         Self {
             points,
             length,
             start_point: start,
+            msg,
         }
     }
 
@@ -45,25 +47,33 @@ impl Pipe {
 
 struct App {
     pipes: Vec<Pipe>,
+    size: (u16, u16),
 }
 
 impl App {
-    fn new() -> Self {
+    fn new(size: (u16, u16), msg: String) -> Self {
         info!("Start a new app ...");
-        let mut s = Self { pipes: vec![] };
+        let mut s = Self {
+            pipes: vec![],
+            size,
+        };
+        info!("terminal size is: {:?}", &size);
 
-        for i in 1..3 {
+        for i in 10..s.size.1 {
             info!("Add pipe number {:?}", i);
+
             let mut p = VecDeque::new();
             let start = Point {
                 x: i * 3,
                 y: 0,
-                hidden: false,
+                // hidden: false,
             };
             p.push_front(start);
 
-            let p = Pipe::new(p, start, (10) as usize);
+            let p = Pipe::new(p, start, 30, msg.clone());
+
             debug!("Push new  pipe {i} {:?}", &p);
+
             s.pipes.push(p);
         }
 
@@ -71,31 +81,25 @@ impl App {
     }
 
     fn run(&mut self) -> io::Result<()> {
-        let size = terminal::size()?;
-
         stdout().queue(terminal::Clear(terminal::ClearType::All))?;
 
         loop {
-            thread::sleep(Duration::from_millis(50));
-            stdout().queue(terminal::Clear(terminal::ClearType::All))?;
+            stdout().queue(terminal::Clear(terminal::ClearType::FromCursorUp))?;
 
             for pipe in self.pipes.iter_mut() {
-                for (i, p) in pipe.points.iter().enumerate() {
+                let msg = pipe.msg.split_whitespace().fold(String::new(), |acc, e| {
+                    let e: String = e.chars().rev().collect();
+                    format!("{acc} {e}")
+                });
+
+                // info!("pipes generated msg {}", msg);
+
+                for (p, m) in pipe.points.iter().zip(msg.chars()) {
                     let mut stdout = stdout();
                     let q = stdout
                         .queue(cursor::MoveTo(p.x, p.y))?
                         .queue(cursor::Hide)?;
-
-                    if p.hidden {
-                        info!("hidden point");
-                        continue;
-                    }
-
-                    if i == 0 {
-                        q.queue(style::PrintStyledContent("●".dark_green()))?;
-                    } else {
-                        q.queue(style::PrintStyledContent("●".dark_green()))?;
-                    }
+                    q.queue(style::PrintStyledContent(m.dark_green()))?;
                 }
 
                 if pipe.points.len() > pipe.length {
@@ -106,7 +110,7 @@ impl App {
                     let mut head = head.clone();
                     head.y += 1;
 
-                    if !pipe.pipe_hit_border(size, &head) {
+                    if !pipe.pipe_hit_border(self.size, &head) {
                         pipe.points.push_front(head);
                     } else {
                         pipe.points.pop_back();
@@ -118,18 +122,19 @@ impl App {
                 }
             }
 
-            thread::sleep(Duration::from_millis(200));
             stdout().flush()?;
+            thread::sleep(Duration::from_millis(200));
         }
     }
 }
 
 fn main() -> io::Result<()> {
+    let msg = "This is my msg that will run".to_string();
+
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
-    let mut app = App::new();
-    info!(">>>>> booting up");
-    warn!(">>>>> booting up");
+    let size = terminal::size()?;
+    let mut app = App::new(size, msg);
     app.run()?;
 
     Ok(())
